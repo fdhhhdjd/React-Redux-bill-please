8616ebce59ef1aa7f56cacaaa4f0afebfa5b461f
tai <nguyentientai10@gmail.com>
2021-08-29 10:07:06 +0700

    push upload

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      <p>okess</p>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
+
+export default Navbar;
